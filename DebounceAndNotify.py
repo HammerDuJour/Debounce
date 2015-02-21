5b7268c331e81dc46d64b71c799f429a4678320f
jfgist <James.Gist@gmail.com>
2015-02-20 21:20:56 -0700

    Update DebounceAndNotify.py

--- DebounceAndNotify.py
+++ DebounceAndNotify.py
@@ -1,10 +1,29 @@
 import datetime
+import RPi.GPIO as GPIO
+import time
 
 
-diff = //2 hours;
-trigger = false;
+#initialize variables
+diff = 0; # set for two hours
+trigger = false;  
 
+#Rasperry Pi GPIO Setup
+inputPin = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(inputPin,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-d1=datetime.now()
-;sleep(1);d2=datetime.now()
-diff=d2-d1
+while True:
+  input = GPIO.input(inputPin)
+  
+  if (input && !trigger):  # set initial values. Our state has changed and we want to start counting time
+    trigger = true;
+    triggerTime = datetime.now()
+  
+  if (input && trigger):
+    now = datetime.now();
+    if (now - triggerTime > diff):
+      #SendEmail()
+      
+  if (!input):
+    trigger = false;
+  
