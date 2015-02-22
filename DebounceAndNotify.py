0d7508e1d862827b10528ac048d880c1df78c215
jfgist <James.Gist@gmail.com>
2015-02-22 15:09:34 -0700

    Update DebounceAndNotify.py

--- DebounceAndNotify.py
+++ DebounceAndNotify.py
@@ -5,8 +5,15 @@ import time
 from textme import *
 
 #initialize variables
+debug = true;
 diff = 2*60*60; # set for two hours
+interval = 60; 
+if (debug):
+  diff = 2*60;
+  interval = 1;
+
 trigger = false;  
+
 
 #Rasperry Pi GPIO Setup
 inputPin = 17
@@ -34,6 +41,6 @@ while True:
     print("Trigger Low, Reset")
     trigger = false;
     
-sleep(60);
+sleep(interval);
 print("Sleep")
   
